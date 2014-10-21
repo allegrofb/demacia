3aaeec7d4ab8bef9ede36f035ab820cc9754579d
jhy <capsulejiang@gmail.com>
2014-10-21 19:08:22 +0800

    October-21, 2014

---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios, '7.0'
+pod 'Appirater', '~> 2.0.2'
+pod 'EAIntroView', '~> 2.6.3'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Appirater
+#define COCOAPODS_POD_AVAILABLE_Appirater
+#define COCOAPODS_VERSION_MAJOR_Appirater 2
+#define COCOAPODS_VERSION_MINOR_Appirater 0
+#define COCOAPODS_VERSION_PATCH_Appirater 4
+
+// EAIntroView
+#define COCOAPODS_POD_AVAILABLE_EAIntroView
+#define COCOAPODS_VERSION_MAJOR_EAIntroView 2
+#define COCOAPODS_VERSION_MINOR_EAIntroView 6
+#define COCOAPODS_VERSION_PATCH_EAIntroView 4
+
