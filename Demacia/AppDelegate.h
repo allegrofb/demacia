4026e249b85e4f72a9c4c643f36495330703bb01
jhy <capsulejiang@gmail.com>
2014-10-21 15:35:00 +0800

    October-21, 2014

--- Demacia/AppDelegate.h
+++ Demacia/AppDelegate.h
@@ -7,11 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MainViewController.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, IChatManagerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (strong, nonatomic) MainViewController *mainController;
 
 @end
 
